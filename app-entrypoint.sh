aeaf833878129bed77114eb3be6be87b98d67dc3
diego1996 <diego.asencio@unillanos.edu.co>
2021-05-25 19:45:19 -0500

    add dump data all.json

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:80
+python manage.py runserver 0.0.0.0:82
 # gunicorn SAP.wsgi:application --bind 0.0.0.0:8081
