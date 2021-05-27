b73e3fcb5caca4ecc848bfccf7fbf182eba647d0
Giver Cupaja <giver.olaya@gmail.com>
2021-05-27 18:31:11 -0500

    update

--- apps/street_lighting/viewsets.py
+++ apps/street_lighting/viewsets.py
@@ -3,6 +3,7 @@ import json
 from django.contrib.auth.models import User
 from django.db.models import Q
 from rest_framework import viewsets, mixins
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from collections import namedtuple
 
