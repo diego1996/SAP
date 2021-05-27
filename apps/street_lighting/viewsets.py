e321517724e9424cbceb3a877761193ea57bc6bc
Giver Cupaja <giver.olaya@gmail.com>
2021-05-27 18:30:02 -0500

    update

--- apps/street_lighting/viewsets.py
+++ apps/street_lighting/viewsets.py
@@ -12,7 +12,22 @@ from apps.street_lighting.serializers import *
 
 Elements = namedtuple('Elements', ('luminarias', 'postes', 'redes', 'camaras', 'transformadores'))
 
+class Pagination(PageNumberPagination):
+    page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'total_pages': self.page.paginator.num_pages,
+            'count': self.page.paginator.count,
+            'results': data
+        })
+
 class ElementsViewSet(viewsets.ViewSet):
+    pagination_class = Pagination
 
     def list(self, request):
         elements = Elements(
