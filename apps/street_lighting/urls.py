c82d965336104f21f41ece6dda8de04c277890f9
Giver Cupaja <giver.olaya@gmail.com>
2021-05-27 17:17:50 -0500

    update

--- apps/street_lighting/urls.py
+++ apps/street_lighting/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from apps.street_lighting.viewsets import *
 
 router = routers.DefaultRouter()
-router.register('elements', ElementsViewSet)
+router.register('elements', ElementsViewSet, basename='Elements')
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
