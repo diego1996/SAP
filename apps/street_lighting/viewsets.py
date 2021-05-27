78a2e3c70d1dfb59049cf83bab4a3e4d9b00a15a
Giver Cupaja <giver.olaya@gmail.com>
2021-05-27 17:15:50 -0500

    update

--- apps/street_lighting/viewsets.py
+++ apps/street_lighting/viewsets.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 from rest_framework import viewsets, mixins
 from itertools import chain
+from django.http import JsonResponse
 
 # ViewSets define the view behavior.
 from apps.street_lighting.models import *
@@ -16,4 +17,4 @@ class ElementsViewSet(mixins.ListModelMixin,
 
     def get_queryset(self):
         result_list = list(chain(Luminaria.objects.all(), Poste.objects.all(), Red.objects.all(), Camara.objects.all(), Transformador.objects.all()))
-        return result_list
+        return JsonResponse(result_list)
